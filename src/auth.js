9266c6052292858811591941bd5f5ac6dd85c388
Aliastrandavid <130996552+Aliastrandavid@users.noreply.github.com>
2025-06-17 19:34:21 +0000

    users JS

---
+++ src/auth.js
@@ -0,0 +1,14 @@
+{
+    "users": [
+        {
+            "id": 1,
+            "username": "david",
+            "name": "TRAN David",
+            "email": "aliastrandavid@gmail.com",
+            "role": "admin",
+            "password": "$2y$10$PVKG5SJcWvP4duB78m6Pm.1jF\/YsHInXOZpDqUqXXyAt.OBPM0j.S",
+            "created_at": "2025-06-13T06:09:17Z",
+            "updated_at": "2025-06-13T06:09:45Z"
+        }
+    ]
+}
